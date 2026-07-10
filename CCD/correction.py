4ee062ebbcd8da1f50ce379fb8f1326870947ae4
Parmenio Boronat Sevilla <parmenio.boronat@uv.es>
2026-07-10 17:24:01 +0200

    Carpeta que conté els fitxers de CCD i els lectors necessaris

---
+++ CCD/correction.py
@@ -0,0 +1,12 @@
+from numpy import interp, nan
+from pandas import read_table
+
+def ccd_correct(xdata, spectra):
+    CCD_Data = read_table('CCD/CCD.csv', delimiter=' ', decimal=',', usecols=[0, 1], header=None)
+    QE = interp(xdata, CCD_Data[0], CCD_Data[1], left=nan, right=nan)
+    QE[QE < 0.05] = nan
+    print(spectra.shape)
+    print(QE.shape)
+    spectra /= QE
+
+    return spectra
